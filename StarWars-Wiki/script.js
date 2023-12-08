c3d83bc25c4fcdec9ce1a98f1e0ebc1d1fea56ec
Anderson <paulinhoanderson.ap@gmail.com>
2023-12-08 20:46:31 -0300

    template StarWars

---
+++ StarWars-Wiki/script.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    fetch('https://swapi.co/api/').then((data) =>{ // nao funciona pois foi excluidondo github
+    }).catch((error) => {
+    alert('Erro ao carregar os filmes!')
+})
+}
